3076471d28228bed0c8e6d425e96eaf3d068c653
David L Barrero <davidlopezbarrero@gmail.com>
2020-10-08 19:35:18 +0200

    Recover files from stash

--- src/main.go
+++ src/main.go
@@ -1,23 +1,21 @@
 package main
 
+
 import (
     "fmt"
-    "os"
-    "io/ioutil"
     "log"
     "net/http"
+    "os"
+    "io/ioutil"
+    "encoding/json"
+
 )
 
-func main() {
-	fmt.Println("Hello, 世界");
-    checkdb();
-    for i:= 0; i < 10; i++ {
-        fmt.Println(i)
-    }
-    return
-}
+// Check if index collection 
 
 const dbName = "db.json"
+
+
 
 
 func checkdb() {
@@ -37,6 +35,7 @@ func checkdb() {
         // Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
 
 }
+    jsonFile, err := os.Open(dbName)
 }
 
 func createfile() {
@@ -46,11 +45,57 @@ func createfile() {
     }
 }
 
-func add(doc string) {
-    return
+
+
+func add(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != "/add" {
+            http.Error(w, "404 not found.", http.StatusNotFound)
+            return
+                                
+        }
+         
+        switch r.Method {
+            case "GET":     
+              http.ServeFile(w, r, "Try with Post")
+            case "POST":
+            // Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+                if  err := r.ParseForm(); err != nil {
+                    fmt.Fprintf(w, "ParseForm() err: %v", err)
+                    return
+                                                                                         
+            }   
+            fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+            name := r.FormValue("name")
+            address := r.FormValue("address")
+            fmt.Fprintf(w, "Name = %s\n", name)
+            fmt.Fprintf(w, "Address = %s\n", address)
+    
+            default:
+            fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+                                                                                                                 
+    }
+            
 }
 
 
-func read(doc string) {
-    return 
+
+
+
+func main() {
+
+    checkdb()
+
+    http.HandleFunc("/add", add)
+    //http.HandleFunc("/delete" , delete)
+    //http.HandleFunc("/read" , read)
+    //http.HandleFunc("/update", update)
+    
+    
+    fmt.Printf("Starting server at port 8080\n")
+            if err := http.ListenAndServe(":8080", nil); err != nil {
+            log.Fatal(err)
+                                            
+    }
+                    
+                    
 }
