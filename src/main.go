0059cccfe3bd544ca4ab2a93869d54ef37a12065
David L Barrero <davidlopezbarrero@gmail.com>
2020-10-18 05:06:06 +0200

    write

--- src/main.go
+++ src/main.go
@@ -34,33 +34,45 @@ func createfile() {
     }
 }
 
-func write() {
-    plan, _ := ioutil.ReadFile(dbName)
-    var data interface{}
-    err := json.Unmarshal(plan, &data)
-    fmt.Println(json.Marshal(err))
-
-    
-}
 
 func read() {
-    data, err := ioutil.ReadFile(dbName)
-    data = string(data)
-    var result map[string]interface{}
+  //Simple Employee JSON which we will parse
+	empArray := `[
+		{
+			"id": 1,
+			"name": "Mr. Boss",
+			"department": "",
+			"designation": "Director"
+		},
+		{
+			"id": 11,
+			"name": "Irshad",
+			"department": "IT",
+			"designation": "Product Manager"
+		},
+		{
+			"id": 12,
+			"name": "Pankaj",
+			"department": "IT",
+			"designation": "Team Lead"
+		}
+	]`
+
+	// Declared an empty interface of type Array
+	var results []map[string]interface{}
 
 	// Unmarshal or Decode the JSON to the interface.
-	json.Unmarshal([]byte(data), &result)
+	json.Unmarshal([]byte(empArray), &results)
 
-	//Reading each value by its key
-	fmt.Println("Id :", result["id"],
-		"\nName :", result["name"],
-		"\nDepartment :", result["department"],
-        "\nDesignation :", result["designation"])
-        
-    if err != nil {
-        fmt.Println("File reading error", err)
-        return
-    }
+	for key, result := range results {
+
+		fmt.Println("Reading Value for Key :", key)
+		//Reading each value by its key
+		fmt.Println("Id :", result["id"],
+			"- Name :", result["name"],
+			"- Department :", result["department"],
+			"- Designation :", result["designation"])
+	}
 }
 
 func put(w http.ResponseWriter, r *http.Request) {
